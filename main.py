62109defab74950a25a48de054568c165d6bda7c
bagpyp <robert@bagpyp.net>
2020-10-21 11:24:57 -0700

    finalize

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 # -*- coding: utf-8 -*-
 """
 Created on Thu Oct 15 14:14:12 2020
@@ -5,6 +6,8 @@ Created on Thu Oct 15 14:14:12 2020
 @author: Bagpyp
 """
 
+from datetime import datetime as dt
+from html2text import html2text
 import json
 from numpy import nan,where
 import pandas as pd
@@ -15,23 +18,19 @@ pd.options.display.width = 180
 pd.options.display.max_colwidth = 40
 import requests
 
-from datetime import datetime as dt
-
 def log(text, stamp=False, base='log'):
     with open(f"logs/{base} {dt.now().strftime('%d-%m-%Y')}.txt", 'a') as file:
         if stamp:
             file.write(f"\n\n\n{dt.now().strftime('%H:%M:%S')}\n\n")
         file.write(text)
         file.write('\n')   
-
-#%%
-
+    
 with open('apiKey.txt') as file:
     a = file.readline()
     b = file.readline()
 
 base = 'https://developer.sidelineswap.com/v1/'
-headers = {'x-api-key':a,
+headers = {'x-api-key':a[:-1],
            'x-client-id':b,
            'accept':'application/json'}
 
@@ -58,11 +57,7 @@ def setAddress(data):
 #   'country': 'US'})
 
 # if res.ok:
-#     print('address set')
-
-
-#%%
-    
+#     print('address set')   
 cmap = pd.read_csv('cat_map.csv')
 det = pd.read_csv('detail_map.csv')
 
@@ -70,6 +65,9 @@ for df in [cmap,det]:
     df.dcsname = df.dcsname.str.replace('hike/pack','hike, pack')
 
 cmap = cmap[cmap.uuid.notna()]
+# remove tabs
+for c in ['uuid','cat']:
+    cmap[c] = cmap[c].str.replace('\t','')
 det = det[det.dcsname.isin(cmap.dcsname)]
 
 c = cmap.set_index('dcsname').cat.to_dict()
@@ -86,6 +84,7 @@ df = df[df.dcsname.isin(list(c.keys()))]
 df[list('dcs')] = df.dcsname.str.split('/',expand=True)
 df['cond'] = where(df.DCSname.str.contains('clearance'),\
                    'used','new')
+# not NEw, the condition
 df.webName = where(df.webName.str.split(' ')\
                    .apply(lambda x: len(x)) == 2,
                    'New: '+df.webName, \
@@ -93,8 +92,17 @@ df.webName = where(df.webName.str.split(' ')\
     
 df.webName = where(df.webName.str.split(' ')\
                    .apply(lambda x: len(x)) == 1,
-                   'From Hillcrest: '+df.webName, \
+                   ' from Hillcrest'+df.webName, \
                    df.webName)    
+# has images only
+df = df.groupby('webName').filter(lambda g: g.image_0.count() > 0)
+
+# strip html
+df.description = df.description.apply(lambda x: html2text(x) if x else x)
+
+# bad prices
+df = df[df.pSale >= 3]
+
 #comment out END to test locally
 df.to_pickle('currentDf.pkl')
 
@@ -102,16 +110,21 @@ df.to_pickle('currentDf.pkl')
 # df = pd.read_pickle('currentDf.pkl')
 #comment our END to run
 
-#%%
+
 req = pd.read_csv('category_fields.csv')
+
+notreq = req[req.required==0]
+notreq = notreq[['catname','field']]
+opFields = {n:f.field.unique().tolist() \
+          for n,f in notreq.groupby('catname', sort=False)}
+
+req = req[req.required==1]
+req = req[['catname','field']]
 fields = {n:f.field.unique().tolist() \
           for n,f in req.groupby('catname', sort=False)}
 
 # print(req.head())
-#%%
 
-
-    
 db = det[['dcsname','dic']].groupby('dcsname',sort=False)
 # ds does not contain 'safety/helmet/ski' as key - fixed
 # ds does not contain 'rental/ski/binding' as key....
@@ -126,15 +139,21 @@ for k in list(c.keys()):
     if k not in ds:
         ds.update({k:{'Condition':'cond'}})
 
-
-
+#apostaphe's
+for s in list('dcs'):
+    df[s] = df[s].str.replace('^mens$',"Men's",regex=True)
+    df[s] = df[s].str.replace('^womens$',"Women's",regex=True)
+    df[s] = df[s].str.replace('^men$',"Men's",regex=True)
+    df[s] = df[s].str.replace('^women$',"Women's",regex=True)
 
 #%%
 gb = df.groupby('webName', sort=False)
 failures= []
 reqs = []
 N = gb.ngroups
+i = 0
 for i,(n,g) in enumerate(gb):
+    i+=1
     print(f'{i}/{N}')
     g = g.to_dict('records')
     g0 = g[0]
@@ -143,7 +162,6 @@ for i,(n,g) in enumerate(gb):
     else:
         g = [g0.copy()]
     cat = c[g0['dcsname']]
-    
     data = {
       "listing_sku": g0['sku'],
       "name": g0['webName'],
@@ -153,52 +171,46 @@ for i,(n,g) in enumerate(gb):
       "model": g0['name'],
       "accepts_offers": True,
       "ship_from_address_id": "db4f7b6a-91d8-4fbb-806a-d07e7449f5e2",
-      "images": [g0[f'image_{i}'] for i in range(5) if g0[f'image_{i}']],
-      # omit
-      "length": 5,
-      "width": 5,
-      "height": 5,
-      "weight": 5,      
-      "items": [
-        {
-          "item_sku": h['sku'],
-          "quantity": h['qty'],
-          "list_price": h['pSale'],
-          "retail_price": h['pMSRP'],
-          "gtin": h['UPC'],
-          "mpn": h['mpn'],
-          "images":[h[f'image_{i}'] for i in range(5) if h[f'image_{i}']],
-          "details": [{'type':k,'option':h[v]} for k,v in ds[h['dcsname']].items()]
-        } for h in g
-      ]
-    }
-    for h in data['items']:
-        # print('original details',h['details'])
+      "images": [g0[f'image_{i}'] for i in range(5) if g0[f'image_{i}']],     
+      "items": []}
+    for h in g:
+        item = {
+            "item_sku": h['sku'],
+            "quantity": h['qty'],
+            "list_price": h['pSale'],
+            "retail_price": h['pMSRP'],
+            "gtin": h['UPC'],
+            "mpn": h['mpn'],
+            "images":[h[f'image_{i}'] for i in range(5) if h[f'image_{i}']],
+            "details": [{'type':k,'option':h[v]} for k,v in ds[h['dcsname']].items()]
+            }
         if cat in fields:
             want = fields[cat]
         else:
-            want = []
-        have = [detail['type'] for detail in h['details']]
+            want = []               
+        if cat in opFields and 'Color' in opFields[cat]:
+            item['details'].extend([{'type':'Color','option':h['color']}])
+        have = [detail['type'] for detail in item['details']]
         need = [x for x in want if x not in have]
         # if Size ends up in need, just grab if from h!!!
-        h['details'].extend([{'type':n,'option':'other'} for n in need])
+        item['details'].extend([{'type':n,'option':'other'} for n in need])
+        data['items'].append(item)
     res = postList(data)
-
+    reqs.append(res)
     if res.status_code != 200:
         failures.append(res)
     else:
         reqs.append(res)
+               
         
-        
-        
-log("FAILURES", stamp=True, base='failureLog')
+log("FAILURES", stamp=True, base='ALLfailureLog')
 for i,f in enumerate(failures):
     logtext = '\n'+'-'*100+f'\n\n{i}\n\n' \
         + json.dumps(json.loads(f.content)) \
         + '\n\n' \
         + json.dumps(json.loads(f.request.body)) \
         + '\n\n'
-    log(logtext, base='failureLog')
+    log(logtext, base='ALLfailureLog')
             
     
     
@@ -223,4 +235,20 @@ for i,f in enumerate(failures):
 #     print('\n', 'RESPONSE')
 #     pprint(json.loads(r.content))
 #     print('\n'+'-'*100+'\n')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
